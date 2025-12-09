94b256c44689ef9472a8e9666133d4cf4e782225
Arvind_Cardr <arvind@cardr.com>
2025-12-09 15:59:02 +0530

    Downgrade Java and Kotlin versions to 17

--- OBDIQAndroidSdk/build.gradle.kts
+++ OBDIQAndroidSdk/build.gradle.kts
@@ -34,17 +34,17 @@ android {
     }
 
     kotlin {
-        jvmToolchain(21)
+        jvmToolchain(17)
     }
 
     android {
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
         kotlinOptions {
-            jvmTarget = "21"
+            jvmTarget = "17"
         }
     }
 
